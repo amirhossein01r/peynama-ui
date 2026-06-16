1961f0910eb2abd3b63983613dde836298362532
AmirhosseinRB <AmirhosseinRB@users.noreply.github.com>
2026-06-16 20:18:25 +0330

    feat: setup theme toggle for storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,12 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview, ReactRenderer } from "@storybook/react-vite";
+import { withThemeByClassName } from "@storybook/addon-themes";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
 
@@ -13,9 +14,18 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
   },
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: "",
+        dark: "dark",
+      },
+      defaultTheme: "dark",
+    }),
+  ],
 };
 
 export default preview;
