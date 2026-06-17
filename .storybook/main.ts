6f8fd3d033c24b375af82c0588ee77f3757320d5
AmirhosseinRB <AmirhosseinRB@users.noreply.github.com>
2026-06-17 18:57:08 +0330

    fix: enable router in Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/tanstack-react";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -9,6 +9,6 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-themes",
   ],
-  framework: "@storybook/react-vite",
+  framework: "@storybook/tanstack-react",
 };
 export default config;
